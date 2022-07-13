e599ffb91cc27047702cec49021d4e7d30803d35
Paul Harvey <Paul Harvey>
2022-07-13 18:20:49 +0100

    Added support for shift registers and reading actual values

--- hex-reader.ino
+++ hex-reader.ino
@@ -1,3 +1,5 @@
+
+#include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include "ValueDisplay.h"
@@ -11,9 +13,22 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 ValueDisplay *valueLeftDisplay;
 ValueDisplay *valueRightDisplay;
 
+#define CLOCK_INHIBIT_PIN 8  // CE pin 15
+#define SERIAL_DATA_IN_PIN 9 // Q9 pin 7
+#define CLOCK_PIN 10         // CP pin 2
+#define LOAD_INHIBIT_PIN 11  // PL pin 1
+
+#define PIN_DELAY_MICROS 5
+
 void setup()
 {
    Serial.begin(9600);
+
+   // Setup 74HC165 pins
+   pinMode(LOAD_INHIBIT_PIN, OUTPUT);
+   pinMode(CLOCK_INHIBIT_PIN, OUTPUT);
+   pinMode(CLOCK_PIN, OUTPUT);
+   pinMode(SERIAL_DATA_IN_PIN, INPUT);
 
    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
@@ -29,13 +44,38 @@ void setup()
    valueRightDisplay = new ValueDisplay(64, 0, &display);
 }
 
+void latchValue()
+{
+   digitalWrite(LOAD_INHIBIT_PIN, LOW);
+   delayMicroseconds(PIN_DELAY_MICROS);
+   digitalWrite(LOAD_INHIBIT_PIN, HIGH);
+   delayMicroseconds(PIN_DELAY_MICROS);
+}
+
+int16_t readByte()
+{
+   int16_t result = 0;
+
+   // Ensure clock is high before getting data - else we'll miss the first bit
+   digitalWrite(CLOCK_INHIBIT_PIN, HIGH);
+   digitalWrite(CLOCK_PIN, HIGH);
+
+   // Grab a byte
+   digitalWrite(CLOCK_INHIBIT_PIN, LOW);
+   result = shiftIn(SERIAL_DATA_IN_PIN, CLOCK_PIN, LSBFIRST) << 8;
+   result |= shiftIn(SERIAL_DATA_IN_PIN, CLOCK_PIN, LSBFIRST);
+   digitalWrite(CLOCK_INHIBIT_PIN, HIGH);
+
+   return result;
+}
+
 void loop()
 {
-   for (byte value = 0; value < 256; value++)
-   {
-      valueLeftDisplay->showValue(value);
-      valueRightDisplay->showValue(255 - value);
+   latchValue();
+   int16_t incoming = readByte();
 
-      delay(100);
-   }
+   valueLeftDisplay->showValue((byte)(incoming >> 8));
+   valueRightDisplay->showValue((byte)incoming);
+
+   delay(500);
 }
